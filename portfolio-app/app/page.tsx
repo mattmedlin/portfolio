f19c51ff9f8e2bbd9128fc6c9164ee0bdb724e16
Matt Medlin <mattmedlin@Matts-MBP.attlocal.net>
2023-07-30 14:59:07 -0500

    change font to lato

---
+++ portfolio-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Nav from "@/components/Nav";
+
+export default function Home() {
+  return (
+    <div className="bg-[#051622]">
+      <Nav></Nav>
+    </div>
+  );
+}
---
+++ portfolio-app/components/Nav.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="w-full">
+      <div className="flex w-11/12 mx-auto border-b-2 border-[#DEB992] h-44 text-[#1BA098] font-sans">
+        <button>Home</button>
+        <button>Projects</button>
+        <button>About</button>
+        <button>Blog</button>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
