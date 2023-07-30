4221ba46c445eb9e6793b28eaa34f885483d33c6
Matt Medlin <mattmedlin@Matts-MBP.attlocal.net>
2023-07-30 15:00:52 -0500

    navbar

--- portfolio-app/components/Nav.tsx
+++ portfolio-app/components/Nav.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Nav() {
   return (
     <div className="w-full">
-      <div className="flex w-11/12 mx-auto border-b-2 border-[#DEB992] h-44 text-[#1BA098] font-sans">
+      <div className="flex w-11/12 mx-auto border-b-2 border-[#DEB992] h-24 text-[#1BA098] font-sans justify-center gap-20">
         <button>Home</button>
         <button>Projects</button>
         <button>About</button>
